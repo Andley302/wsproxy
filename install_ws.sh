e198c05a8cc1fa0b74a32e41f656b2a7747d453d
Andley S <39422775+Andley302@users.noreply.github.com>
2021-09-09 12:15:02 -0300

    Update install_ws.sh

--- install_ws.sh
+++ install_ws.sh
@@ -6,11 +6,13 @@ if [ -d "$DIR" ]; then
   # Take action if $DIR exists. #
   echo "Installing config files in ${DIR}..."
   cd /etc/SSHPlus;
+  rm -rf wsproxy.py
   wget https://raw.githubusercontent.com/Andley302/wsproxy/main/wsproxy.py;
   echo -e "netstat -tlpn | grep -w 80 > /dev/null || {  screen -r -S 'wsproxy' -X quit;  screen -dmS wsproxy python /etc/SSHPlus/wsproxy.py 80; }" >>/etc/autostart;
   netstat -tlpn | grep -w 80 > /dev/null || {  screen -r -S 'wspy' -X quit;  screen -dmS wspy python /etc/SSHPlus/wsproxy.py 80; };
 else
   cd /etc/CrashVPN;
+  rm -rf wsproxy.py
   wget https://raw.githubusercontent.com/Andley302/wsproxy/main/wsproxy.py;
   echo -e "netstat -tlpn | grep -w 80 > /dev/null || {  screen -r -S 'wsproxy' -X quit;  screen -dmS wsproxy python /etc/SSHPlus/wsproxy.py 80; }" >>/etc/autostart;
   netstat -tlpn | grep -w 80 > /dev/null || {  screen -r -S 'wspy' -X quit;  screen -dmS wspy python /etc/SSHPlus/wsproxy.py 80; };
