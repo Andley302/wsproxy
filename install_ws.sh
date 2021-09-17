383c43dc12f940f8fc3ef877353c8c9fe3398dec
Andley S <39422775+Andley302@users.noreply.github.com>
2021-09-17 14:58:14 -0300

    Update install_ws.sh

--- install_ws.sh
+++ install_ws.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 apt update && apt install wget -y;
+wget https://raw.githubusercontent.com/Andley302/utils/main/antcrashws.sh -O /bin/antcrashws.sh > /dev/null 2>&1
+chmod +x /bin/antcrashws.sh;
 echo -e "INSTALADOR WS PROXY"
 DIR="/etc/SSHPlus"
 if [ -d "$DIR" ]; then
@@ -8,13 +10,15 @@ if [ -d "$DIR" ]; then
   cd /etc/SSHPlus;
   rm -rf wsproxy.py
   wget https://raw.githubusercontent.com/Andley302/wsproxy/main/wsproxy.py;
-  echo -e "netstat -tlpn | grep -w 80 > /dev/null || {  screen -r -S 'wsproxy' -X quit;  screen -dmS wsproxy python /etc/SSHPlus/wsproxy.py 80; }" >>/etc/autostart;
+  #echo -e "netstat -tlpn | grep -w 80 > /dev/null || {  screen -r -S 'wsproxy' -X quit;  screen -dmS wsproxy python /etc/SSHPlus/wsproxy.py 80; }" >>/etc/autostart;
+  echo -e "netstat -tlpn | grep -w 80 > /dev/null || screen -dmS wsproxy antcrashws.sh 80" >>/etc/autostart;
   netstat -tlpn | grep -w 80 > /dev/null || {  screen -r -S 'wspy' -X quit;  screen -dmS wspy python /etc/SSHPlus/wsproxy.py 80; };
 else
   cd /etc/CrashVPN;
   rm -rf wsproxy.py
   wget https://raw.githubusercontent.com/Andley302/wsproxy/main/wsproxy.py;
-  echo -e "netstat -tlpn | grep -w 80 > /dev/null || {  screen -r -S 'wsproxy' -X quit;  screen -dmS wsproxy python /etc/CrashVPN/wsproxy.py 80; }" >>/etc/autostart;
+  #echo -e "netstat -tlpn | grep -w 80 > /dev/null || {  screen -r -S 'wsproxy' -X quit;  screen -dmS wsproxy python /etc/CrashVPN/wsproxy.py 80; }" >>/etc/autostart;
+  echo -e "netstat -tlpn | grep -w 80 > /dev/null || screen -dmS wsproxy antcrashws.sh 80" >>/etc/autostart;
   netstat -tlpn | grep -w 80 > /dev/null || {  screen -r -S 'wspy' -X quit;  screen -dmS wspy python /etc/CrashVPN/wsproxy.py 80; };
 fi
 
