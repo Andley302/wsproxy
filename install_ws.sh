de8ede1a3c9078eb34e0effc73b2430fad685d03
Andley S <39422775+Andley302@users.noreply.github.com>
2022-01-06 12:40:51 -0300

    Update install_ws.sh

--- install_ws.sh
+++ install_ws.sh
@@ -2,17 +2,27 @@
 apt update && apt install wget -y;
 wget https://raw.githubusercontent.com/Andley302/utils/main/antcrashws.sh -O /bin/antcrashws.sh > /dev/null 2>&1
 chmod +x /bin/antcrashws.sh;
-echo -e "INSTALADOR WS PROXY"
+wget https://raw.githubusercontent.com/Andley302/utils/main/antcrashsockssquid.sh -O /bin/antcrashsockssquid.sh > /dev/null 2>&1
+chmod +x /bin/antcrashsockssquid;
+wget https://raw.githubusercontent.com/Andley302/utils/main/antcrashsocksdirect.sh -O /bin/antcrashsocksdirect.sh > /dev/null 2>&1
+chmod +x /bin/antcrashsocksdirect;
+echo -e "INSTALADOR SOCKS PROXY"
 DIR="/etc/SSHPlus"
 if [ -d "$DIR" ]; then
   # Take action if $DIR exists. #
   echo "Installing config files in ${DIR}..."
   cd /etc/SSHPlus;
   rm -rf wsproxy.py
+  rm -rf proxy.py
   wget https://raw.githubusercontent.com/Andley302/wsproxy/main/wsproxy.py;
+  wget https://raw.githubusercontent.com/Andley302/wsproxy/main/proxy.py;
   #echo -e "netstat -tlpn | grep -w 80 > /dev/null || {  screen -r -S 'wsproxy' -X quit;  screen -dmS wsproxy python /etc/SSHPlus/wsproxy.py 80; }" >>/etc/autostart;
   echo -e "netstat -tlpn | grep -w 80 > /dev/null || screen -dmS wsproxy antcrashws.sh 80" >>/etc/autostart;
   netstat -tlpn | grep -w 80 > /dev/null || {  screen -r -S 'wspy' -X quit;  screen -dmS wspy python /etc/SSHPlus/wsproxy.py 80; };
+  echo -e "netstat -tlpn | grep -w 8080 > /dev/null || screen -dmS proxy antcrashsocksdirect.sh 8080" >>/etc/autostart;
+  netstat -tlpn | grep -w 8080 > /dev/null || screen -dmS proxy antcrashsocksdirect.sh 8080
+  echo -e "netstat -tlpn | grep -w 8088 > /dev/null || screen -dmS proxy antcrashsockssquid.sh 8088" >>/etc/autostart;
+  netstat -tlpn | grep -w 8088 > /dev/null || screen -dmS proxy antcrashsockssquid.sh 8088
 else
   cd /etc/CrashVPN;
   rm -rf wsproxy.py
