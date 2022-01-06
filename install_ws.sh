f4f4564963d93520fd7bec531aeb01b333f70a41
Andley S <39422775+Andley302@users.noreply.github.com>
2022-01-06 13:38:31 -0300

    Update install_ws.sh

--- install_ws.sh
+++ install_ws.sh
@@ -18,11 +18,11 @@ if [ -d "$DIR" ]; then
   wget https://raw.githubusercontent.com/Andley302/wsproxy/main/proxy.py;
   #echo -e "netstat -tlpn | grep -w 80 > /dev/null || {  screen -r -S 'wsproxy' -X quit;  screen -dmS wsproxy python /etc/SSHPlus/wsproxy.py 80; }" >>/etc/autostart;
   echo -e "netstat -tlpn | grep -w 80 > /dev/null || screen -dmS wsproxy antcrashws.sh 80" >>/etc/autostart;
-  netstat -tlpn | grep -w 80 > /dev/null || {  screen -r -S 'wspy' -X quit;  screen -dmS wspy python /etc/SSHPlus/wsproxy.py 80; };
+  netstat -tlpn | grep -w 80 > /dev/null || {  screen -r -S 'wspy' -X quit;  screen -dmS wsproxy python /etc/SSHPlus/wsproxy.py 80; };
   echo -e "netstat -tlpn | grep -w 8080 > /dev/null || screen -dmS socks_direct antcrashsocksdirect.sh 8080" >>/etc/autostart;
-  netstat -tlpn | grep -w 8080 > /dev/null || screen -dmS socks_direct antcrashsocksdirect.sh 8080
+   netstat -tlpn | grep -w 8080 > /dev/null || {  screen -r -S 'socks_direct' -X quit;  screen -dmS socks_direct python /etc/SSHPlus/wsproxy.py 8080; };
   echo -e "netstat -tlpn | grep -w 8088 > /dev/null || screen -dmS socks_squid antcrashsockssquid.sh 8088" >>/etc/autostart;
-  netstat -tlpn | grep -w 8088 > /dev/null || screen -dmS socks_squid antcrashsockssquid.sh 8088
+   netstat -tlpn | grep -w 8088 > /dev/null || {  screen -r -S 'socks_squid' -X quit;  screen -dmS socks_squid python /etc/SSHPlus/wsproxy.py 8088; };
 else
   cd /etc/CrashVPN;
   rm -rf wsproxy.py
